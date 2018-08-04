fa55f9e35e4d3df9671ad0fb151ae463b8e6fa50
Tim <timwoo112@gmail.com>
2018-08-04 17:30:19 -0400

    Wanted to start with just node and mongodb

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require ('mongoose');
-
-//connect to mongoose
-mongoose.connect('mongodb://localhost.world', { useNewUrlParser: true });
-var db = mongoose.connection;
-
-app.get('/', function(req, res){
-  res.send('Please use /api/world');
-});
-
-app.listen(3000);
-console.log('Running on port 3000');
+var mongo = require('mongodb');

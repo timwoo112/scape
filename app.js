74759fc7591fbe1c714236476cb03aeb87862f88
Tim <timwoo112@gmail.com>
2018-08-05 02:45:06 -0400

    Implemented update for money value

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 const url = 'mongodb+srv://twoolley:test@cluster0-jjfxc.mongodb.net/test?retryWrites=true';
 
 // Database Name
-const dbName = 'test';
+const dbName = 'world_state';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
@@ -13,6 +13,13 @@ MongoClient.connect(url, function(err, client) {
   const db = client.db(dbName);
   //put commands under this line
   console.log("Connected successfully to server");
+
+  var myquery = { name: "Tim" };
+  var newvalues = {$set: {money: "5000"} };
+  db.collection("player").updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+  });
 
   client.close();
 });

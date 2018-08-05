3f55aabf6def53dc4736478d5a6d1c7e61819e6b
Tim <timwoo112@gmail.com>
2018-08-05 03:58:53 -0400

    Added readMoney function

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ MongoClient.connect(url, function(err, client) {
 */
 
 // This function allows you to update the player's money.
-// It takes an integer variable that is the new amount. 
+// It takes an integer variable that is the new amount.
 function addMoney(amount){
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
@@ -46,4 +46,21 @@ function addMoney(amount){
     client.close();
   });
 }
+
+// Function that reads the current amount of money player has.
+function readMoney(){
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    const db = client.db(dbName);
+    //put commands under this line
+    console.log("Connected successfully to server");
+
+      db.collection("player").findOne({}, function(err, result) {
+      if (err) throw err;
+      console.log(result.money);
+      client.close();
+    });
+  });
+}
 addMoney(6767);
+readMoney();

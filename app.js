dfb5f15cc4b782b519a04bf90fea3c876b559ed5
Tim <timwoo112@gmail.com>
2018-08-05 00:24:43 -0400

    Stable server

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://twoolley:test@cluster0-jjfxc.mongodb.net/test?retryWrites=true';
 
 // Database Name
-const dbName = 'myproject';
+const dbName = 'test';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
-  console.log("Connected successfully to server");
-
   const db = client.db(dbName);
+  //put commands under this line
+  console.log("Connected successfully to server");
 
   client.close();
 });

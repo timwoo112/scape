28f38af345f2d6b388abf0af31274326e00d44e0
Tim <timwoo112@gmail.com>
2018-08-05 03:15:30 -0400

    Created an addMoney function to update the player's money amount.

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const url = 'mongodb+srv://twoolley:test@cluster0-jjfxc.mongodb.net/test?retryWr
 // Database Name
 const dbName = 'world_state';
 
-// Use connect method to connect to the server
+// Example of a simple update to the server.
+// This has been deprecated because I put it into a function.
+/*
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
   const db = client.db(dbName);
@@ -23,3 +25,25 @@ MongoClient.connect(url, function(err, client) {
 
   client.close();
 });
+*/
+
+// This function allows you to update the player's money.
+// It takes an integer variable that is the new amount. 
+function addMoney(amount){
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    const db = client.db(dbName);
+    //put commands under this line
+    console.log("Connected successfully to server");
+
+    var myquery = { name: "Tim" };
+    var newvalues = {$set: {money: amount} };
+    db.collection("player").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+    });
+
+    client.close();
+  });
+}
+addMoney(6767);

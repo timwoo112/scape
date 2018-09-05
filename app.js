08ff42a49323627526bc6c205ea23760fe00b8fa
Tim <timwoo112@gmail.com>
2018-09-05 09:59:59 -0400

    Made a single call to the db to get player object

--- app.js
+++ app.js
@@ -14,65 +14,31 @@ const url = 'mongodb+srv://twoolley:test@cluster0-jjfxc.mongodb.net/test?retryWr
 const dbName = 'world_state';
 var tester = null;
 
-function readMoney() {
+function getPlayerData() {
   return new Promise(function(resolve, reject) {
-    MongoClient.connect(url, function(err, client) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
       assert.equal(null, err);
       const db = client.db(dbName);
-      //put commands under this line
-      console.log("Connected from readMoney");
-
-      db.collection("player").findOne({}, function(err, result) {
-        if (err)
+      console.log("Connecting from getPlayerData");
+      db.collection("player").findOne({"name": "Tim"}, function(err, result) {
+        if (err) {
           throw err;
-        console.log(result.money);
-        resolve(result.money);
-        console.log("Closing connection from readMoney")
+          reject("oops");
+        } else {
+          console.log(result);
+          resolve(result);
+          client.close();
+          console.log("Closing connection from getPlayerData")
+        }
       });
     });
   });
 }
 
-// Read location function. Returns an array of coordinates.
-function readLocation() {
-  return new Promise(function(resolve, reject) {
-    MongoClient.connect(url, function(err, client) {
-      assert.equal(null, err);
-      const db = client.db(dbName);
-      //put commands under this line
-      console.log("Connecting from readLocation");
-
-      db.collection("player").findOne({}, function(err, result) {
-        if (err)
-          throw err;
-        resolve(result.location);
-        console.log("Closing connection from readLocation")
-      });
-    });
-  });
-}
-
-// Try out the await keyword
-function combineAllFunctions() {
-  return new Promise(function(resolve, reject) {
-    readMoney().then(function(data) {
-      user.playerMoney = data;
-      console.log("Test inside" + user.playerMoney);
-    }, function(err) {
-      console.log("Error! " + err);
-    });
-
-    readLocation().then(function(data) {
-      user.playerLocation = data;
-      console.log(user.playerLocation);
-    }, function(err) {
-      console.log("Error! " + err);
-    });
-    resolve("Success!!!");
-  });
-}
 // usage
-combineAllFunctions().then(function() {
+getPlayerData().then(function(value) {
+  console.log("Getting the carryover variable")
+  console.log(value);
   console.log("The player money is " + user.playerMoney);
   console.log("The player location is" + user.playerLocation);
 }, function(err) {
